cdfb4c97b3cc7eebf976a19d1694a3ad0487c016
siddhama <b19196@students.iitmandi.ac.in>
2023-05-20 15:41:23 +0530

    Added files for training GAN inversion

---
+++ scripts/dist_train_Indianfaces_encoder.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "=================================================="
+echo "Please ensure you have installed the requirements!"
+
+Download data.
+echo "Downloading data ..."
+mkdir -p data/
+wget -nv https://www.dropbox.com/s/9kqsip3jg7bmhjr/cropped_images.zip?dl=0 \
+     -O data/cropped_images.zip --quiet
+
+# Launch training.
+echo "Launch training job with 1 GPU."
+echo "=================================================="
+PORT=6666 ./scripts/dist_train.sh 1 \
+    configs/stylegan_Indianfaces_encoder_y.py \
+    work_dirs/stylegan_Indianfaces_encoder     
+# GPUS=$1
+# CONFIG=$2
+# WORK_DIR=$3
+# PORT=${PORT:-29500}
+
+# python -m torch.distributed.launch \
+#        --nproc_per_node=8 \
+#        --master_port=${PORT} \
+#        ./train.py configs/stylegan_Indianfaces256.py \
+#            --work_dir work_dirs/stylegan_Indianfaces_train \
+#            --launcher="pytorch" \
+#            ${@:4}
